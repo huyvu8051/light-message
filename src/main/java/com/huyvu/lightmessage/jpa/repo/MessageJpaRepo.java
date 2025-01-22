1702de41cfc1db804b7dc87ec7b588760820a03b
huyvu8051 <huyvu8051@gmail.com>
2025-01-22 16:09:47 +0700

    sick my

--- src/main/java/com/huyvu/lightmessage/jpa/repo/MessageJpaRepo.java
+++ src/main/java/com/huyvu/lightmessage/jpa/repo/MessageJpaRepo.java
@@ -8,6 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface MessageJpaRepo extends JpaRepository<Message, Long> {
+
+/*
+    EXPLAIN ANALYZE
+    SELECT DISTINCT ON (conv_id) *
+    FROM message
+    WHERE conv_id in (1, 2)
+    ORDER BY conv_id, send_at DESC;
+    */
     @Query("""
     select new com.huyvu.lightmessage.entity.MessageEntity(id, conv.id, content, sender.id, sendAt )
     from Message
