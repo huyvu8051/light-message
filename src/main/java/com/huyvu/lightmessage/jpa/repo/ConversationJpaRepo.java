5330e63d03dce0e59499ab839d56759ca440183c
huyvu8051 <huyvu8051@gmail.com>
2025-01-25 22:27:11 +0700

    align query

--- src/main/java/com/huyvu/lightmessage/jpa/repo/ConversationJpaRepo.java
+++ src/main/java/com/huyvu/lightmessage/jpa/repo/ConversationJpaRepo.java
@@ -9,25 +9,25 @@ import java.util.List;
 
 public interface ConversationJpaRepo extends JpaRepository<Conversation, Long> {
     @Query(value = """
-           SELECT conv_ids.conv_id AS conv_id
-                , conv.name
-                , conv.is_group_chat
-                , m.id             AS m_id
-                , m.content
-                , m.send_at
-             FROM (SELECT conversation_id
-                     FROM member
-                    WHERE user_id = :userId) AS conv_ids(conv_id)
-                      LEFT JOIN LATERAL (
-                 SELECT *
-                   FROM message
-                  WHERE conv_id = conv_ids.conv_id
-                  ORDER BY send_at DESC
-                  LIMIT 1
-                 ) m ON TRUE
-                      LEFT JOIN conversation conv
-                                ON conv.id = conv_ids.conv_id
-            """, nativeQuery = true)
+                   SELECT conv_ids.conv_id AS conv_id
+                        , conv.name
+                        , conv.is_group_chat
+                        , m.id             AS m_id
+                        , m.content
+                        , m.send_at
+                     FROM (SELECT conversation_id
+                             FROM member
+                            WHERE user_id = :userId) AS conv_ids(conv_id)
+                              LEFT JOIN LATERAL (
+                         SELECT *
+                           FROM message
+                          WHERE conv_id = conv_ids.conv_id
+                          ORDER BY send_at DESC
+                          LIMIT 1
+                         ) m ON TRUE
+                              LEFT JOIN conversation conv
+                                        ON conv.id = conv_ids.conv_id
+                   """, nativeQuery = true)
     List<Tuple> findNewestConversation(long userId);
 
 
